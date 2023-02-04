a9269f5d75a16f7e2d22ca9f51861074794cfe7e
NurCeylin <ceylincetin03@hotmail.com>
2023-02-04 15:20:42 +0300

    abut
    a

--- Assets/Scripts/Password.cs
+++ Assets/Scripts/Password.cs
@@ -6,49 +6,168 @@ using UnityEngine.SceneManagement;
 
 public class Password : MonoBehaviour
 {
+    private string[] password = {"0","0","0","0"};
+    private string[] correct = {"1","1","1","1"};
 
-    // public Buttons buttons;
-    private string input;
-    private int Pass;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public void Enter(){
+       if(password[0]==correct[0] && password[1]==correct[1] && password[2]==correct[2] && password[3]==correct[3]){
+         Debug.Log("doğru");
+       }
+
+    }
+
+
+
+    public void GetInput1(){
         
-    }
+        if(password[0] == "0"){
+            password[0] = "1";
+            Debug.Log("a");
+        }
+        else if(password[1] == "0"){
+            password[1] = "1";
+            Debug.Log("b");
+        }
+        else if(password[2] == "0"){
+            password[2] = "1";
+            Debug.Log("c");
+        }
+        else if(password[3] == "0"){
+            password[3] = "1";
+            Debug.Log("d");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+    Debug.Log("ss");
+    }
+    
+    public void GetInput2(){
         
-    }
-
-
-    public void ReadStringInput(string s){
-        input = s;
-        Debug.Log(input);
-        if(input == "1234"){
-            Pass = 1;
-            if(Pass == 1){
-                Debug.Log("Seden");
-                
-
-              // buttons.Meet_Open();
-              // buttons.Meet_password_Close();
-
-               // buttons.SwitchScene4();
-               SceneManager.LoadScene(3);
-            }    
-       
+        if(password[0] == "0"){
+            password[0] = "2";
         }
-        else if(input!= "1234"){
-            Debug.Log("Ceylin");
-
-            //buttons.Meet_password_Close();
-           
+        else if(password[1] == "0"){
+            password[1] = "2";
+        }
+        else if(password[2] == "0"){
+            password[2] = "2";
+        }
+        else if(password[3] == "0"){
+            password[3] = "2";
         }
     }
+    
+    public void GetInput3(){
+        
+        if(password[0] == "0"){
+            password[0] = "3";
+        }
+        else if(password[1] == "0"){
+            password[1] = "3";
+        }
+        else if(password[2] == "0"){
+            password[2] = "3";
+        }
+        else if(password[3] == "0"){
+            password[3] = "3";
+        }
+    }
+   
+    public void GetInput4(){
+        
+        if(password[0] == "0"){
+            password[0] = "4";
+        }
+        else if(password[1] == "0"){
+            password[1] = "4";
+        }
+        else if(password[2] == "0"){
+            password[2] = "4";
+        }
+        else if(password[3] == "0"){
+            password[3] = "4";
+        }
+    }
 
+    public void GetInput5(){
+        
+        if(password[0] == "0"){
+            password[0] = "5";
+        }
+        else if(password[1] == "0"){
+            password[1] = "5";
+        }
+        else if(password[2] == "0"){
+            password[2] = "5";
+        }
+        else if(password[3] == "0"){
+            password[3] = "5";
+        }
+    }
 
+    public void GetInput6(){
+        
+        if(password[0] == "0"){
+            password[0] = "6";
+        }
+        else if(password[1] == "0"){
+            password[1] = "6";
+        }
+        else if(password[2] == "0"){
+            password[2] = "6";
+        }
+        else if(password[3] == "0"){
+            password[3] = "6";
+        }
+    }
 
+    public void GetInput7(){
+        
+        if(password[0] == "0"){
+            password[0] = "7";
+        }
+        else if(password[1] == "0"){
+            password[1] = "7";
+        }
+        else if(password[2] == "0"){
+            password[2] = "7";
+        }
+        else if(password[3] == "0"){
+            password[3] = "7";
+        }
+    }
+
+    public void GetInput8(){
+        
+        if(password[0] == "0"){
+            password[0] = "8";
+        }
+        else if(password[1] == "0"){
+            password[1] = "8";
+        }
+        else if(password[2] == "0"){
+            password[2] = "8";
+        }
+        else if(password[3] == "0"){
+            password[3] = "8";
+        }
+    }
+
+    public void GetInput9(){
+        
+        if(password[0] == "0"){
+            password[0] = "9";
+        }
+        else if(password[1] == "0"){
+            password[1] = "9";
+        }
+        else if(password[2] == "0"){
+            password[2] = "9";
+        }
+        else if(password[3] == "0"){
+            password[3] = "9";
+        }
+    }
 }
+
+  
