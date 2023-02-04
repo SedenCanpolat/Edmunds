009206823fc7bd54dcc6d196ea1e9f5b8c9c4d76
Seden1 <114922611+Seden1@users.noreply.github.com>
2023-02-04 10:34:11 +0300

    DiaOver
    dd

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -19,6 +19,15 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0)){
+            if(textComponent.text == lines[index]){
+                NextLine();
+            }
+        }
+        else{
+            StopAllCoroutines();
+            textComponent.text = lines[index];
+        }
         
     }
 
@@ -32,6 +41,17 @@ public class Dialogue : MonoBehaviour
         foreach(char c in lines[index].ToCharArray()){
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine(){
+        if(index < lines.Length - 1){
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else{
+            gameObject.SetActive(false);
         }
     }
 
