b6e07da1c76be9eac5096d1e230a7489a1572cf8
SedenCanpolat <sedencanpolat@gmail.com>
2025-04-23 14:14:42 +0300

    last update

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+   
+    void Awake() { // start da Ok
+       // DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}
